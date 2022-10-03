dc83fa3be87a83203378c3a059b9a931380e3202
Kannan Padmanabhan <79042757+kannan1202@users.noreply.github.com>
2022-10-03 19:58:35 +0530

    Update Assignment 7.py

--- DSA Assignments/Assignment 7.py
+++ DSA Assignments/Assignment 7.py
@@ -1,3 +1,4 @@
+#Question 3:
 from collections import Counter
 import heapq
 def MostFrequentPurchase(purchases):
@@ -10,3 +11,19 @@ purchases = ['yellowShirt', 'redHat', 'blackShirt', 'bluePants', 'redHat','pinkH
 'greenPants', 'greenPants', 'greenPants']
 result = MostFrequentPurchase(purchases)
 print(f"The most frequent purchase of the day is {result}")
+
+"""Question 4: An almost sorted array is given to us and the task is to sort that array completely. Then,
+which sorting algorithm would you prefer and why?[Salesforce] """
+
+""" 
+The answer is insertion sort.
+let us take a example of an almost sorted array , [10,20,30,40,50,5]
+Here time complexity depends upon the number of comparison and the number of swaps.
+In above example the array is sorted until the element 50, so the number of comparison until the element 50 will be 1 and swaps until the element 50 will be zero,
+as the array is already sorted.
+When the element is 5, there will be n-1 number of comparisons and n-1 number of swaps.
+In case of implementation, until the element 50 , it doesn't go into while loop.When the element is 5, it executes while loop n-1 number of times.
+
+Hence, Time complexity is equal to number of comparison + number of swaps
+=> (n-1) + (n-1)
+=> O(n)
