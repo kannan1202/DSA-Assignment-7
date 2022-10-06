f0d52022b00f1486742bf6d64a3adce69fc9a8c9
Kannan Padmanabhan <79042757+kannan1202@users.noreply.github.com>
2022-10-06 21:48:29 +0530

    Add files via upload

--- DSA Assignments/Assignment 5.py
+++ DSA Assignments/Assignment 5.py
@@ -5,14 +5,15 @@ the integer part of the result is returned. Also, talk about the time complexity
 code.
 
 """
-# import math
-# def SquareRoot(num):
-#     return int(math.sqrt(num))
-#
-# num = int(input("Enter a number to compute square root: "))
-# result = SquareRoot(num)
-# print(f"The square root of {num} is {result}")
-# # Time complexity O(1)
+# Time complexity O(1)
+import math
+def SquareRoot(num):
+    return int(math.sqrt(num))
+
+num = int(input("Enter a number to compute square root: "))
+result = SquareRoot(num)
+print(f"The square root of {num} is {result}")
+
 
 """
 Question 2: You are a product manager and currently leading a team to develop a new product.
@@ -23,35 +24,35 @@ which causes all the following ones to be bad. Also, talk about the time complex
 your code.
 
 """
-#Time complexity O(logn)
-# def FirstBadOne(Version,left,right):
-#     if left == right:
-#         return left
-#     else:
-#         while left<=right:
-#             middle = left +(right - left)//2
-#             if Version[middle] == 0:
-#                 return FirstBadOne(Version,middle+1,right)
-#             else:
-#                 return FirstBadOne(Version,left,middle-1)
-#
-# Version = [0,0,0,1,1,1,1,1,1]
-# left = 0
-# right = len(Version)-1
-# result = FirstBadOne(Version,left,right)
-# print(f"The index of the first bad version is {result}")
+# Time complexity O(logn), because by using binary search we divide the array everytime by 2,
+# So we get the time complexity as logn to the base 2,when solved using substitution method or master's theorem.
+def FirstBadOne(Version,left,right):
+    if left == right:
+        return left
+    else:
+        while left<=right:
+            middle = left +(right - left)//2
+            if Version[middle] == 0:
+                return FirstBadOne(Version,middle+1,right)
+            else:
+                return FirstBadOne(Version,left,middle-1)
+
+Version = [0,0,0,1,1,1,1,1,1]
+left = 0
+right = len(Version)-1
+result = FirstBadOne(Version,left,right)
+print(f"The index of the first bad version is {result}")
 
 """
 Question 3: Given a positive integer num, write a program that returns True if num is a perfect
 square else return False. Do not use built-in functions like sqrt. Also, talk about the time
 complexity of your code.
 """
-
+#Time complexity O(n)
 def PerfectSquare(num):
-    res = num**0.5
-    print(res)
-    if res == type(int):
-        return True
+    for i in range(1,num+1):
+        if i*i == num:
+            return True
     else:
         return False
 
